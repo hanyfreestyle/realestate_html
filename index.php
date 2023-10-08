4edeb3883ad002d2792a3f43bd14a8cbf9276594
Hany Darwish <hany.freestyle4u@gmail.com>
2023-10-08 10:23:13 +0300

    test

--- index.php
+++ index.php
@@ -6,17 +6,18 @@ $PageMenu = 'index';
 ?>
 
 <body>
+hi
 <?php
-require_once 'inc_preloader.php';
-
-#require_once 'inc_header_top.php';
-require_once 'inc_header_menu.php';
-
-require_once 'page_home.php';
-
-
-require_once 'inc_footer.php';
-FooterMenu("index");
+//require_once 'inc_preloader.php';
+//
+//#require_once 'inc_header_top.php';
+//require_once 'inc_header_menu.php';
+//
+//require_once 'page_home.php';
+//
+//
+//require_once 'inc_footer.php';
+//FooterMenu("index");
 ?>
 
 <?php
